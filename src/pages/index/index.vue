375f6b613f569e82d9fa2fed3a76317019869901
soumns <2794122209@qq.com>
2022-09-29 14:27:14 +0800

    🎉 init-mini-cli

---
+++ src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="container"> Hello </view>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {}
+  },
+  onLoad() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
